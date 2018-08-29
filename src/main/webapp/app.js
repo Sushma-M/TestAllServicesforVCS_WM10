d50102a40eafd467d4080c57ba1bbc50f6860b77
niranjan andhe <niranjan.andhe@wavemaker.com>
2018-08-29 07:38:20 +0000

    Initial Push

---
+++ src/main/webapp/app.js
@@ -0,0 +1,26 @@
+App.onPageReady = function (activePageName, activePageScope, $activePageEl) {
+    App.onAppVariablesReady();
+};
+
+/* perform any action on the variables within this block(on-page-load) */
+
+App.onAppVariablesReady = function () {
+    /*
+     * variables can be accessed through 'App.Variables' property here
+     * e.g. App.Variables.staticVariable1.getData()
+     */
+
+};
+
+/* perform any action on session timeout here, e.g clearing some data, etc */
+
+App.onSessionTimeout = function () {
+    /*
+     * NOTE:
+     * On re-login after session timeout:
+     * if the same user logs in(through login dialog), app will retain its state
+     * if a different user logs in, app will be reloaded and user is redirected to respective landing page configured in Security.
+     */
+
+};
+
---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,15 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestAllServicesforVCS_WM10",
+  "homePage" : "Main",
+  "name" : "TestAllServicesforVCS_WM10",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0",
+  "xsrf_header_name" : "X-WM-XSRF-TOKEN"
+};
